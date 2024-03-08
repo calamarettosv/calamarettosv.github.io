3bdffeb8f16a4bb19476e546b8ace7ef4bea42ad
calamarettosv <161667984+calamarettosv@users.noreply.github.com>
2024-03-08 17:01:13 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 function toggleMenu() {
-    var menu = document.querySelector('.menu');
-    menu.classList.toggle('show');
+  var menu = document.querySelector(".menu");
+  menu.classList.toggle("show");
 }
 
-document.addEventListener('click', function(event) {
-    var menu = document.querySelector('.menu');
-    var menuToggle = document.querySelector('.menu-toggle');
+document.addEventListener("click", function (event) {
+  var menu = document.querySelector(".menu");
+  var menuToggle = document.querySelector(".menu-toggle");
 
-if (!menu.contains(event.target) && !menuToggle.contains(event.target) && menu.classList.contains('show')) {
-    menu.classList.remove('show');
+  if (
+    !menu.contains(event.target) &&
+    !menuToggle.contains(event.target) &&
+    menu.classList.contains("show")
+  ) {
+    menu.classList.remove("show");
   }
 });
+
+if (window.location.hostname.match(/^www\./i)) {
+  window.location.hostname = window.location.hostname.replace(/^www\./i, "");
+}
